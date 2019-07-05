cf957190facc66379865977d870d3eaac14cc94d
Till Neunast <tilln@users.noreply.github.com>
2019-07-05 21:41:24 +1200

    Configurable response code validation; More validations

--- src/main/java/nz/co/breakpoint/jmeter/iso8583/ISO8583TestElement.java
+++ src/main/java/nz/co/breakpoint/jmeter/iso8583/ISO8583TestElement.java
@@ -2,7 +2,10 @@ package nz.co.breakpoint.jmeter.iso8583;
 
 import org.apache.jmeter.testbeans.TestBean;
 
-/* Marker interface for GUI classes
+/* Marker interface for GUI classes plus some common constants
  */
 public interface ISO8583TestElement extends TestBean {
+    int RESPONSE_CODE_FIELD_NO = 39,
+        PIN_FIELD_NO = 52,
+        MAC_FIELD_NO = 64;
 }
